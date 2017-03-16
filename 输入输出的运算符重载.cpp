bd2bfb2ac9e577e5ab3b24a73711206028fdbf26
JUXINbit <632464297@qq.com>
2017-03-16 09:16:16 +0800

    3.16b变动

--- 输入输出的运算符重载.cpp
+++ 输入输出的运算符重载.cpp
@@ -19,7 +19,7 @@ public:
 	}
 };
  ostream&  operator<<(ostream& os,const Date& d)//cout是ostream里面的一个对象，而ostream是一个类，输出类；  os是一个临时对象；//为什么是這种形式我不能很容易的去理解
-{
+{                                               //不使用void是为了能够连续输入输出；
 	os << d._year << "-" << d._month << "-" << d._day<< endl;
 	return os;//必须返回一个值，但是我不太清楚這里面的原理，之前重载+-的时候都是return *this;那又是为什么；
 }
