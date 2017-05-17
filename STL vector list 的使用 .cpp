abd944330d78e54f762224936316c22741346c86
JUXINbit <632464297@qq.com>
2017-05-17 23:55:20 +0800

    STL 中顺序表和链表的使用

--- STL vector list 的使用 .cpp
+++ STL vector list 的使用 .cpp
@@ -8,95 +8,63 @@
 using namespace std;
 //1. vector的基本使用
 
-//int main()
-//{
-//	vector<int> v1;
-//	v1.push_back(1);
-//	v1.push_back(2);
-//	v1.push_back(3);
-//	v1.push_back(4);
-//	v1.push_back(5);
-//	v1.push_back(6);
-//	vector<int>::iterator it1 = v1.begin();    //刚开始我想不通过迭代器实现打印vector 后来发现根本没有东西来遍历啊，也就明白迭代器的作用是很重要的
-//	while (it1 != v1.end())
-//	{
-//		cout << *it1 << " ";
-//		it1++;
-//	}
-//	cout << endl;
-//	//cout << *(v1.end()-1) << endl;
-//	return 0;
-//}
-
-//将print函数写在函数外面 ，這样更加方便
-//void PrintMyVector(const vector<int>& v)             //当我使用const修饰的时候 vector里面的迭代器也必须要使用const_iterator 不然没法使用
-//{
-//	vector<int>::const_iterator  it = v.begin();      //const的配套使用很重要
-//	while (it != v.end())
-//	{
-//		cout << *it << " ";
-//		it++;
-//	}
-//	cout << endl;
-//}
-//template <class T>
-//void PrintMyVector(vector<T>& v)                 //对模板的理解和使用   把打印函数定义成模板函数 這样的话打印就更加方便了     
-//{
-//	vector<T>::iterator  it = v.begin();
-//	while (it != v.end())
-//	{
-//		//*it = 1;             //迭代器的对象能够对vector的元素进行修改
-//		cout << *it << " ";
-//		it++;
-//	}
-//	cout << endl;
-//}
-//再简化一点
-template <class T>
-void PrintMyVector(T& v)                 
+template<class T>
+void Print(vector<T>& v)
 {
-	T::iterator  it = v.begin();
-	while (it != v.end())
+	vector<T>::iterator itv = v.begin();
+	while (itv != v.end())
 	{
-		//*it = 1;            
-		cout << *it << " ";
-		it++;
+		cout << *itv << " ";
+		itv++;
 	}
 	cout << endl;
+
 }
-//int main()
-//{
-//	vector<int> v1;
-//	v1.push_back(1);
-//	v1.push_back(2);
-//	v1.push_back(3);
-//	v1.push_back(4);
-//	v1.push_back(5);
-//	PrintMyVector(v1);
-//	vector<string> v2;
-//	v2.push_back("hh");
-//	v2.push_back("jj");
-//	v2.push_back("kk");
-//	PrintMyVector(v2);
-//	/*vector<int> v2;
-//	v2.push_back(4);
-//	v2.push_back(5);
-//	v2.push_back(6);
-//	v2.push_back(7);
-//	PrintMyVector(v2);*/
-//	return 0;
-//}
+int main()
+{
+	vector<int> v1;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v1.push_back(4);
+	v1.push_back(5);
+	v1.push_back(6);
+	v1.push_back(7);
+	v1.push_back(8);
+	vector<int>::iterator itv = v1.begin();
+	while (itv!=v1.end())
+	{
+		cout << *itv << " ";
+		itv++;
+	}
+	cout << endl;
+	Print(v1);
+	cout << v1.capacity() << endl;
+	//v1.reserve(10);                       //注意reserve的使用
+	//cout << v1.capacity() << endl;
+	v1.resize(12);
+	Print(v1);
+	cout << v1.capacity() << endl;
+	v1.resize(5);
+	Print(v1);
+	cout << v1.capacity() << endl;
+	v1.resize(5, 4);                //[*] 这里给值没有反应,意思就是我只有扩大了元素的元素如果其中有没有给值的resize的第二个参数才起作用
+	Print(v1);
+	v1.resize(12, 6);
+	Print(v1);
+	v1.reserve(2);      //容量扩大了之后就没法往小了降，这语句是不起作用的。
+}
 
 
 
 //2.std::list
 //类似于vector
-int main()
-{
-	list<int> l1;
-	l1.push_back(1);
-	l1.push_back(1);
-	l1.push_back(1);
-	PrintMyVector(l1);
-	return 0;
-}
+//int main()
+//{
+//	list<int> l1;
+//	l1.push_back(1);
+//	l1.push_back(1);
+//	l1.push_back(1);
+//	PrintMyVector(l1);
+//	return 0;
+//}
