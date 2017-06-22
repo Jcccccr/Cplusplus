c28ec970403927df3cc3f7367d0177b22ee46537
JUXINbit <632464297@qq.com>
2017-06-22 23:16:59 +0800

    bloom 过滤器

---
+++ Bloom Filter.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include"Bloom.h"
+
+template<class K,class V>
+class BloomFilter
+{
+private:
+	bitmap  _bloomf;
+public:
+	void InitBloom
+	{
+    }
+	void Setbloom()
+	{
+	}
+	void Resetbloom()
+	{
+	}
+	void Find()
+	{
+	}
+protected:
+	size_t HashStr()
+	{
+	}
+};
+void   testBloomFilter()
+{
+
+}
+int main()
+{
+	testBloomFilter();
+	return 0;
+}
