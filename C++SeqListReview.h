536d28d5c8a30bad069ee7ac1a2fb3baa00bfbd2
JUXINbit <632464297@qq.com>
2017-07-14 00:00:10 +0800

    顺序表的回顾练习

---
+++ C++SeqListReview.h
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#ifndef __C++SEQLISTREVIEW_H__
+#define __C++SEQLISTREVIEW_H__
+#endif
+
+#include<iostream>
+#include<assert.h>
+using namespace std;
+
+
+typedef int Datatype;
+class SeqList
+{
+private:
+	Datatype* _a;
+	size_t _size;
+	size_t _capacity;
+public:
+	SeqList()                 //构造
+	{}
+	SeqList(const SeqList& s)   //拷贝构造
+	{
+
+	}
+	void CheckCapacity()
+	{
+	
+	}
+	void PushBack(const Datatype& data)
+	{
+
+	}
+	void Print()
+	{
+	
+	}
+};
