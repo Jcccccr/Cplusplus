9a67211cd7468071e223b1a983157184972271f8
JUXINbit <632464297@qq.com>
2017-07-20 00:21:50 +0800

    字符串查找（（版本2）

--- SearchInString.cpp
+++ SearchInString.cpp
@@ -4,32 +4,40 @@
 //比如：“abcdefabcdefabc”中第一个只出现两次为‘d’，要求时间复杂度为O(N)，空间复杂度为O(1)
 
 //方案一：使用一个26个字母的数组，数组中单元的位置上放置出现的字母的个数，然后像文件压缩那样，遇到一个字母就找相应的数组的位置然后+1，
-//当加完了之后检查一下数组中的值，如果是2 直接退出完成查找。
-
+//遍历一遍之后就得到了所有出现的字母的次数统计，然后注意了： 遍历原来的字符串，检查每一个字符出现的次数，要是是2个就返回
 #include<iostream>
 #include<string>
+#include<assert.h>
 
 using namespace std;
-
 char  str[26] = { 0 };
 char SearchInString(char *p)
 {
+	assert(p);
 	size_t i = 0;
 	while (p[i] != '\0')
 	{
 		str[p[i] - 'a']++;
-		if (str[p[i] - 'a'] == 2)
-		{
-			return p[i];
-		}
 		++i;
 	}
-	return 'a';
+	size_t j = 0;
+	while (p[j] != '\0')
+	{
+		if (str[p[j] - 'a'] == 2)
+		{
+			return p[j];
+		}
+		j++;
+	}
+	printf("没有只出现两次的字母\n");
+	return 0;
 }
 void test()
 {
-	char*  p = "abcdefabcdefabc";
-	cout<<SearchInString(p);
+	//char*  p = "abjcmkdjefjabcdefakbcm";
+	//char*  p = "akbjbkbjsudni";
+	char*  p = "";
+	cout << SearchInString(p) << endl;
 }
 int main()
 {
