c6e6625bf046340a06e07d9ae0d0ec64071dabc8
JUXINbit <632464297@qq.com>
2017-07-21 23:49:44 +0800

    二叉树层序

---
+++ BinaryTreeLevelOrder.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//二叉树的层序遍历
+//利用队列
+#include<iostream>
+#include<queue>
+#include<string>
+#include<assert.h>
+using namespace std;
+
+template <class T>
+void LevelOrder(Node* root)
+{
+	assert(root);
+	queue<int> _q;
+	_q.push(root);
+	while (!_q.empty())
+	{
+        
+	}
+   
+}
+void test()
+{
+
+}
+int main()
+{
+	return 0;
+}
